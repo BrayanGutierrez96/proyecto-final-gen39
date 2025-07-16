685abaaca21d01420f5a46ee2702232c700aa6ba
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2025-07-15 21:41:10 -0500

    Estructurando: Se crean los ejemplos de estructuras de carpetas moleculares.BG

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+import Navbar from '../components/navbar/Navbar'
+
+function Home() {
+  return (
+    <main>
+        <Navbar />
+        <h1>Home</h1>
+    </main>
+  )
+}
+
+export default Home
