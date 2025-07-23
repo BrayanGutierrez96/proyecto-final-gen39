261745086c2cfc0797fc08f30d3c4debf2a3c95c
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2025-07-22 21:23:16 -0500

    Desarrollo: Agrego el banner con su test unitario. BG

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,13 @@
 import Categories from '../components/categories/Categories'
 import Navbar from '../components/navbar/Navbar'
 import {Beneficios} from '../components/Beneficios/Beneficios'  
+import Banner from '../components/banner/Banner'
 
 
 function Home() {
   return (
     <main>
-        
+        <Banner/>
         <Categories />
         <Beneficios />
     </main>
