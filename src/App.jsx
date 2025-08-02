19c2a7fd5a9947908e0a76bb108457442d891870
cristal <cristalvillon00@gmail.com>
2025-08-02 13:15:38 -0400

    agregar Login.jsx, RegisterForm y dependencias nuevas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import Home from "./pages/Home"
-
-
-function App() {
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/navbar/Navbar.jsx'
+import Home from './pages/Home'
+import Login from './pages/Login.jsx'
+export default function App() {
   return (
     <div>
-      <Home/>
+      <Navbar />
+      <div className="flex">
+        <main className="flex-1 p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Login />} />
+          </Routes>
+        </main>
+      </div>
     </div>
-
   )
 }
-
-export default App
